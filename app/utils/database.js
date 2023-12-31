c89d45f02caa66269357a203064e0370f1a1518a
takeishi-g <89860661+takeishi-g@users.noreply.github.com>
2023-12-31 16:51:28 +0900

    fix

--- app/utils/database.js
+++ app/utils/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try{
-    await mongoose.connect(process.env.MONGOOSE_AUTH)
+    await mongoose.connect(process.env.NEXT_PUBLIC_MONGOOSE_AUTH)
     console.log("Success: Connected to MongoDB")
   } catch(err) {
     console.log("Failure: Unconnected to MongoDB")
