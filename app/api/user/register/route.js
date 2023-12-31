09f7083bab235ca4621a4341286287205bb0a640
takeishi-g <89860661+takeishi-g@users.noreply.github.com>
2023-12-31 18:25:13 +0900

    fix

--- app/api/user/register/route.js
+++ app/api/user/register/route.js
@@ -2,11 +2,11 @@ import { NextResponse } from "next/server";
 import connectDB from "@/app/utils/database";
 import { UserModel } from "@/app/utils/schemaModels";
 
-export const corsHeaders = {
-  "Access-Control-Allow-Origin": process.env.NEXT_PUBLIC_URL,
-  "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type",
-}
+// export const corsHeaders = {
+//   "Access-Control-Allow-Origin": process.env.NEXT_PUBLIC_URL,
+//   "Access-Control-Allow-Methods": "POST, OPTIONS",
+//   "Access-Control-Allow-Headers": "Content-Type",
+// }
 
 export async function POST(request) {
   const reqBody = await request.json()
@@ -14,7 +14,7 @@ export async function POST(request) {
     await connectDB()
     await UserModel.create(reqBody)
     return NextResponse.json({
-      headers: corsHeaders,
+      // headers: corsHeaders,
       message: "ユーザー登録成功"
     })
   } catch(err) {
