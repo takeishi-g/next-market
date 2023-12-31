c544ce583d736d43ee2822c5423be5f31c65a4f2
takeishi-g <89860661+takeishi-g@users.noreply.github.com>
2023-12-31 16:57:27 +0900

    fix

--- app/api/item/readall/route.js
+++ app/api/item/readall/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import connectDB from "@/app/utils/database";
-import { ItemModel } from "@/app/utils/schemaModels";
+import connectDB from "../../../utils/database";
+import { ItemModel } from "../../../utils/schemaModels";
 
 export async function GET() {
   try {
